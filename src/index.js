b48b8177718cf5b35718a19146e5ef513b94ed5a
NotHunterr <notryoshi@gmail.com>
2023-05-13 00:23:29 -0700

    Backbone of Discord Bot

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 const { Client, IntentsBitField } = require('discord.js')
+require('dotenv').config()
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+})
+
+client.on('ready', (c) => {
+  console.log(`${c.user.tag} Is online. :D`)
+})
+
+client.on('messageCreate', (msg) => {
+  console.log(
+    'Username: ' + msg.author.username + '#' + msg.author.discriminator
+  )
+  console.log('Message Content: ' + msg.content)
+  console.log('Time of Creation: ' + msg.createdAt)
+})
+
+client.on('messageCreate', (msg) => {
+  if (msg.author.bot) {
+    return
+  }
+
+  if (msg.content === 'I love Brook so much!') {
+    msg.reply("She's the Bestest and Coolest Girlfriend Ever!!!")
+  }
+})
+
+client.login(process.env.DISCORD_TOKEN)
